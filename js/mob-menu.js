a1019a979b3279551cb5dfff9eeffa34462447be
OlegSubotin <subotinoleg0@gmail.com>
2022-05-01 13:36:18 +0300

    header-menu

---
+++ js/mob-menu.js
@@ -0,0 +1,11 @@
+const openBtn = document.getElementById('burger-open');
+const closeBtn = document.getElementById('close-btn');
+const menuEl = document.getElementById('data-menu');
+
+openBtn.addEventListener('click', () => {
+    menuEl.classList.toggle('is-hidden');
+});
+
+closeBtn.addEventListener('click', () => {
+    menuEl.classList.toggle('is-hidden');
+})
